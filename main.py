80578f660b485243e00099d92ebcb7c03116f988
David García <ccristhiangarcia@gmail.com>
2018-09-02 18:06:13 -0500

    Cookies

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import make_response
 from flask_wtf import CsrfProtect
 
 import forms
@@ -17,6 +18,12 @@ def login():
     title = 'Curso de Flask'
     return render_template('login.html', title = title, form = login_form)
 
+@app.route('/cookie')
+def cookie():
+    response = make_response( render_template('cookie.html') )
+    response.set_cookie('customize_cookie', 'flask_facilito')
+    return response
+
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     comment_form = forms.CommentForm(request.form)
@@ -26,6 +33,9 @@ def index():
         print comment_form.comment.data
     else:
         print 'Error en el formulario'
+
+    customize_cookie = request.cookies.get('customize_cookie', 'Undefined')
+    print customize_cookie
     title = 'Curso de Fask'
     return render_template('index.html', title = title, form = comment_form)
 
